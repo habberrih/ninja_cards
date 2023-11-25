be7b49dec93a5b523dcb9996771b51056f736eee
habberrih <abdul2013ly@gmail.com>
2023-11-25 11:03:42 +0200

    feat: add HomeApp widget

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (BuildContext context) => const HomeApp(),
+    ),
+  );
+}
+
+class HomeApp extends StatelessWidget {
+  const HomeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
